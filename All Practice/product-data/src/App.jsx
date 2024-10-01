658d526257059a8ebf18abd7353e625862626b93
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-10-01 20:32:29 +0530

    React Practice

--- All Practice/product-data/src/App.jsx
+++ All Practice/product-data/src/App.jsx
@@ -1,14 +1,14 @@
-import AllProducts from "./Components/Allproducts";
-import FilteredProduct from "./Components/Filteredproduct";
+import ProductRendering from "./components/ProductRendering";
 
 const App = () => {
   return (
     <div className="container">
-      <h1 className="text-center mt-4 mb-5 bg-info text-light rounded-5">
+      <h1 className="text-center mt-5 mb-5 bg-info rounded-5 p-3 text-light fw-bold">
         All Products
       </h1>
-      <AllProducts />
-      <FilteredProduct />
+      <div className="row row-gap-4">
+        <ProductRendering />
+      </div>
     </div>
   );
 };
