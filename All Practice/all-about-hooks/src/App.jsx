286d028bfe195412b53a56d4301ff0061e503906
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-10-06 18:34:02 +0530

    React Practice

--- All Practice/all-about-hooks/src/App.jsx
+++ All Practice/all-about-hooks/src/App.jsx
@@ -1,10 +1,22 @@
+import ProductCounter from "./components/hooks/usestate/ProductCounter";
 import Counter from "./components/hooks/usestate/Counter";
+import BackgroundColors from "./components/hooks/usestate/BackgroundColors";
 
 const App = () => {
   return (
     <div className="container">
-      <h1 className="text-center text-4xl mb-10 mt-10 bg-green-400 text-white font-bold p-3 rounded-full">-: Counter :-</h1>
+      <h1 className="text-center text-4xl mb-10 mt-10 bg-green-400 text-white font-bold p-3 rounded-full">
+        -: Counter :-
+      </h1>
       <Counter />
+      <h1 className="text-center text-4xl mb-16 mt-16 bg-green-400 text-white font-bold p-3 rounded-full">
+        -: All Products :-
+      </h1>
+      <ProductCounter />
+      <h1 className="text-center text-4xl mb-16 mt-16 bg-green-400 text-white font-bold p-3 rounded-full">
+        -: BackGround Colors :-
+      </h1>
+      <BackgroundColors />
     </div>
   );
 };
---
+++ All Practice/all-about-hooks/src/components/hooks/usestate/BackgroundColors.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { colors } from "../../ProductData";
+
+const BackgroundColors = () => {
+  const [index, setIndex] = useState(0);
+
+  const backgroundColors = colors[index];
+  console.log(backgroundColors);
+
+  const handlePreviousButton = () =>
+    index > 0 ? setIndex(index - 1) : setIndex(colors.length - 1);
+
+  const handleNextButton = () =>
+    index < backgroundColors.length - 1 ? setIndex(index + 1) : setIndex(0);
+
+  return (
+    <div className="parent flex justify-center items-center">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handlePreviousButton}
+      >
+        Previous
+      </button>
+      <div
+        className="child mx-7"
+        style={{ backgroundColor: backgroundColors }}
+      ></div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleNextButton}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default BackgroundColors;
