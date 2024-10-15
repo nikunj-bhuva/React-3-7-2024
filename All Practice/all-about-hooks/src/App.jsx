f9844a42033e71bb23f5d96130cf31ab63c03d58
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-10-16 03:54:32 +0530

    React Practice

--- All Practice/all-about-hooks/src/App.jsx
+++ All Practice/all-about-hooks/src/App.jsx
@@ -4,6 +4,7 @@ import BackgroundColors from "./components/hooks/usestate/BackgroundColors";
 import State from "./components/hooks/usestate/State";
 import LiftState from "./components/hooks/usestate/LiftState";
 import FormControl from "./components/hooks/usestate/FormControl";
+import ToggleSwitcher from "./components/hooks/usestate/ToggleSwitcher";
 
 const App = () => {
   return (
@@ -25,10 +26,14 @@ const App = () => {
         -: Lift State :-
       </h1>
       <LiftState /> */}
-      <h1 className="text-center text-4xl mb-16 mt-16 bg-zinc-500 text-white font-bold p-3 rounded-full">
+      {/* <h1 className="text-center text-4xl mb-16 mt-16 bg-zinc-500 text-white font-bold p-3 rounded-full">
         Form Control
       </h1>
-      <FormControl />
+      <FormControl /> */}
+      <h1 className="text-center text-4xl mb-16 mt-16 bg-zinc-500 text-white font-bold p-3 rounded-full">
+        Toggle Switcher
+      </h1>
+      <ToggleSwitcher />
     </div>
   );
 };
---
+++ All Practice/all-about-hooks/src/components/hooks/usestate/ToggleSwitcher.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./ToggleSwitcher.module.css";
+
+const ToggleSwitcher = () => {
+  // initial state value of isOn //
+
+  const [isOn, setIsOn] = useState(false);
+
+  // function to handle toggle button //
+
+  const handleToggleSwitch = () => {
+    setIsOn(!isOn);
+  };
+
+  return (
+    <div
+      className={`${
+        isOn ? styles["toggle-switch-on"] : styles["toggle-switch"]
+      }`}
+      onClick={handleToggleSwitch}
+    >
+      <div className={`${isOn ? styles["switch-on"] : styles["switch-off"]}`}>
+        <span className="switch-state">{isOn ? "ON" : "OFF"}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleSwitcher;
